ca87eb60436dcf0905169739c83caadde821fe02
Lucky Indra Efendi <90430786+LuckyIndraEfendi@users.noreply.github.com>
2022-11-19 20:37:38 +0700

    Create AnimeAPi.js

---
+++ AnimeAPi.js
@@ -0,0 +1,10 @@
+ [
+    {
+        tittle : `Date a Live IV`,
+        image : `asset/img/top anime/Date-A-Live-IV.png`,
+    },
+    {
+        tittle : `Kawaii dake ja Nai Shikimori-san`,
+        image : `asset/img/top anime/Kawaii-dake-ja-Nai-Shikimori-san.webp`,
+    }
+]
